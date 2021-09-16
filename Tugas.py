e8d809b81153ef29031977cf297fb3ac4db36f79
Aditya Wardianto <adityawardianto.19073@mhs.its.ac.id>
2021-09-17 01:39:35 +0700

    Test Data Path 2

--- Tugas.py
+++ Tugas.py
@@ -13,46 +13,46 @@ class Main:
         # File Loading and Padding
         root = os.path.dirname(sys.argv[0])
         st.write(root)
-        # data1, data2 = self.file_loader()
-        # self.data1_len, self.data2_len = self.length(data1, data2)
-        # data2_padded = self.padder(data2)
+        data1, data2 = self.file_loader()
+        self.data1_len, self.data2_len = self.length(data1, data2)
+        data2_padded = self.padder(data2)
 
-        # st.title("Cross Correlation")
-        # st.caption("Aditya Wardianto 07311940000001 - Biomodelling")
+        st.title("Cross Correlation")
+        st.caption("Aditya Wardianto 07311940000001 - Biomodelling")
 
-        # col1, col2 = st.columns(2)
-        # # Time Lag
+        col1, col2 = st.columns(2)
+        # Time Lag
 
-        # with st.echo():
-        #     self.t_lag = st.slider(label="Time Lag", min_value=-(self.data2_len-1), max_value=self.data2_len -
-        #                            1, value=0, help="Slider to shift Data 2 position horizontaly")
-        #     data2_out = data2_padded[self.data2_len -
-        #                              self.t_lag:2*self.data2_len-self.t_lag]
+        with st.echo():
+            self.t_lag = st.slider(label="Time Lag", min_value=-(self.data2_len-1), max_value=self.data2_len -
+                                   1, value=0, help="Slider to shift Data 2 position horizontaly")
+            data2_out = data2_padded[self.data2_len -
+                                     self.t_lag:2*self.data2_len-self.t_lag]
 
-        #     # Plotting Input
-        #     chart_input = pd.DataFrame(np.hstack(
-        #         (data1.reshape(-1, 1), data2_out.reshape(-1, 1))), columns=['Data 1', 'Data 2'])
-        #     st.line_chart(chart_input)
+            # Plotting Input
+            chart_input = pd.DataFrame(np.hstack(
+                (data1.reshape(-1, 1), data2_out.reshape(-1, 1))), columns=['Data 1', 'Data 2'])
+            st.line_chart(chart_input)
 
-        # # Correlation
-        # correlation = self.correlate(data1, data2_padded)
+        # Correlation
+        correlation = self.correlate(data1, data2_padded)
 
-        # # Plotting Correlation
-        # chart_output = pd.DataFrame(correlation, columns=['Correlation'])
-        # st.line_chart(chart_output)
+        # Plotting Correlation
+        chart_output = pd.DataFrame(correlation, columns=['Correlation'])
+        st.line_chart(chart_output)
 
     @st.cache(allow_output_mutation=True)
     def file_loader(self):
 
-        root = os.path.dirname(sys.argv[0])
-        path1 = r'Data/test.txt'
-        path2 = r'Data/test2.txt'
+        # root = os.path.dirname(sys.argv[0])
+        path1 = r'/app/Data/test.txt'
+        path2 = r'/app/Data/test2.txt'
 
-        full_path1 = os.path.join(root, path1)
-        full_path2 = os.path.join(root, path2)
+        # full_path1 = os.path.join(root, path1)
+        # full_path2 = os.path.join(root, path2)
 
-        data1 = np.loadtxt(full_path1)
-        data2 = np.loadtxt(full_path2)
+        data1 = np.loadtxt(path1)
+        data2 = np.loadtxt(path2)
 
         return data1, data2
 
