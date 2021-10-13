5004c3751a87ca79a9c42b0ae864733e24e03da7
Aditya Wardianto <adityawardianto.19073@mhs.its.ac.id>
2021-10-13 20:09:07 +0700

    Added Numba and Performance Handling

--- backend_linear_prediction.py
+++ backend_linear_prediction.py
@@ -2,8 +2,10 @@ import numpy as np
 import streamlit as st
 import scipy.io.wavfile as wav
 import os
+from numba import jit, njit
 
-@st.cache
+
+@st.cache(max_entries=10, ttl=600)
 def _load_data(data_path):
     load_data = np.loadtxt(data_path)
     raw_data_x = load_data[:, 0]
@@ -12,7 +14,7 @@ def _load_data(data_path):
     return raw_data_x, raw_data_y, padded_data_y
 
 
-@st.cache
+@st.cache(max_entries=10, ttl=600)
 def _auto_correlation(M, raw_data_y, padded_data_y):
     # Meyimpan data raw
     data = raw_data_y
@@ -27,116 +29,148 @@ def _auto_correlation(M, raw_data_y, padded_data_y):
     return auto_cor_matrix
 
 
+@jit(forceobj=True, looplift=True)
+def _get_rxx_matrix(rxx):
+    padded_rxx = np.pad(rxx[:-1], (len(rxx[:-1]) - 1, 0), "reflect")
+    rxx_matrix = np.lib.stride_tricks.sliding_window_view(padded_rxx, len(rxx[:-1]))
+    rxx_matrix = np.flip(rxx_matrix, axis=0)
+    return rxx_matrix
+
+
+@jit(forceobj=True, looplift=True)
+def _x_matrix_maker(padded_data_y, raw_data_y, M, a_inv_t, offset=0):
+    x = np.lib.stride_tricks.sliding_window_view(
+        padded_data_y[int(len(raw_data_y) - M) : int(len(raw_data_y) * 2 - offset)],
+        len(a_inv_t),
+    )
+    x = np.flip(x, axis=1)
+    return x.T
+
+
+@njit
+def _inverse_matrix(rxx_matrix):
+    return np.linalg.inv(rxx_matrix)
+
+
+@njit
+def _get_e_m(a_inv_t, x):
+    out = np.dot(a_inv_t.reshape(1, -1).astype(np.float64), x.astype(np.float64))
+    return out
+
+
+@njit
+def _get_x_predict(a, x, e_m):
+    return np.dot(
+        a.reshape(1, -1).astype(np.float64), x.astype(np.float64)
+    ).flatten() + e_m.astype(np.float64)
+
+
+@njit
+def _mse(error_modelling):
+    return np.sum(np.square(error_modelling.flatten())) / (
+        len(error_modelling.flatten()) - 1
+    )
+
+
+@njit
+def _mag_theta(M, a_coef):
+    theta = np.arange(0, np.pi, 0.01).reshape(1, -1)
+    theta_coef = np.arange(1, M + 1).reshape(-1, 1) * theta
+    min_j_theta_coef = -1j * theta_coef
+    exp_min_j_theta_coef = np.exp(min_j_theta_coef)
+    a_coef_dot = np.dot(
+        a_coef.reshape(1, -1).astype(np.complex64),
+        exp_min_j_theta_coef.astype(np.complex64),
+    )
+    H_z = 1 / (1 - a_coef_dot)
+    mag_h_z = np.abs(H_z)
+    return theta, mag_h_z
+
+
+@njit
+def _norm_to_wav(raw_data_y, x_predict):
+    raw_data = raw_data_y.flatten()
+    predicted_data = x_predict.flatten()
+    min_r_d = np.min(raw_data)
+    max_r_d = np.max(raw_data)
+    min_p_d = np.min(predicted_data)
+    max_p_d = np.max(predicted_data)
+    new_max = 2147483647
+    new_min = -2147483648
+
+    norm_raw_data = (raw_data - min_r_d) / (max_r_d - min_r_d) * (
+        new_max - new_min
+    ) + new_min
+    norm_predicted_data = (predicted_data - min_p_d) / (max_p_d - min_p_d) * (
+        new_max - new_min
+    ) + new_min
+
+    return norm_raw_data, norm_predicted_data
+
+
 class LinearModelPrediction:
     def __init__(self, data_path) -> None:
         self.raw_data_x, self.raw_data_y, self.padded_data_y = _load_data(data_path)
 
     def find_rxx_matrix(self, M):
         self.M = M
-        auto_cor_matrix = _auto_correlation(
-            M, raw_data_y=self.raw_data_y, padded_data_y=self.padded_data_y
-        )
+
         # Mencari rxx
-        self.rxx = auto_cor_matrix[: int(M) + 1, 0]
+        self.rxx = _auto_correlation(
+            M, raw_data_y=self.raw_data_y, padded_data_y=self.padded_data_y
+        )[: int(M) + 1, 0]
 
-        # Mencari matriks Rxx-------
-
-        #  Membuat rxx(0->P-1) terpadding
-        padded_rxx = self.symetric_padding(self.rxx[:-1])
-        rxx_matrix = np.lib.stride_tricks.sliding_window_view(
-            padded_rxx, len(self.rxx[:-1])
-        )
-        rxx_matrix = np.flip(rxx_matrix, axis=0)
-        self.rxx_matrix = rxx_matrix
-
-    def symetric_padding(self, data):
-        return np.pad(data, (len(data) - 1, 0), "reflect")
+        # Mencari matriks Rxx-
+        self.rxx_matrix = _get_rxx_matrix(self.rxx)
 
     def find_a_coef(self):
 
         # Membuat inverse matriks rxx
-        self.inv_rxx_matrix = np.linalg.inv(self.rxx_matrix)
+        self.inv_rxx_matrix = _inverse_matrix(self.rxx_matrix)
 
-        self.a_coef = np.dot(self.inv_rxx_matrix, self.rxx[1:].reshape(-1, 1))
+        self.a_coef = np.dot(self.inv_rxx_matrix, self.rxx[1:].reshape(-1, 1)).flatten()
 
     def find_e_m(self):
         # Membuat matrix a inverse transpose
-        a_inv_t = -self.a_coef.flatten()
-        a_inv_t = np.insert(a_inv_t, 0, 1, axis=0)
-        self.a_inv_t = a_inv_t
+        self.a_inv_t = np.insert(-self.a_coef, 0, 1, axis=0)
         # Membuat matriks agar dapat dikali dengan a invers transpose
         # bentuk akhir
         # x(0) x(1) x(2)
         # x(-1) x(0) x(1)
         # X(-2) x(-1) x(0)
 
-        x = self.x_matrix_maker(a_inv_t)
+        x = _x_matrix_maker(self.padded_data_y, self.raw_data_y, self.M, self.a_inv_t)
 
         # Mencari error
-        self.e_m = np.dot(a_inv_t.reshape(1, -1), x)
-
-    def x_matrix_maker(self, a_inv_t, offset=0):
-        x = np.lib.stride_tricks.sliding_window_view(
-            self.padded_data_y[
-                int(len(self.raw_data_y) - self.M) : int(
-                    len(self.raw_data_y) * 2 - offset
-                )
-            ],
-            len(a_inv_t),
-        )
-
-        x = np.flip(x, axis=1)
-        return x.T
+        self.e_m = _get_e_m(self.a_inv_t, x)
 
     def find_x_predict(self):
-        a = self.a_coef.flatten()
 
-        x = self.x_matrix_maker(a, offset=1)
+        x = _x_matrix_maker(
+            self.padded_data_y, self.raw_data_y, self.M, self.a_coef, offset=1
+        )
 
-        self.x_predict = np.dot(a.reshape(1, -1), x).flatten() + self.e_m
+        self.x_predict = _get_x_predict(self.a_coef, x, self.e_m)
 
     def find_error_modelling(self):
         self.error_modelling = self.raw_data_y - self.x_predict
 
     def find_mse(self):
-        self.mse = np.sum(np.square(self.error_modelling.flatten())) / (
-            len(self.error_modelling.flatten()) - 1
-        )
+        self.mse = _mse(self.error_modelling)
 
     def find_freq_resp(self):
-        theta = np.arange(0, 2 * np.pi, 0.01).reshape(1, -1)
-
-        real_cos_coef = np.arange(self.M + 1).reshape(-1, 1)
-        real_cos_theta_coef = np.cos(theta * real_cos_coef)
-        real = np.square(np.dot(self.a_inv_t, real_cos_theta_coef))
-
-        imaj_cos_coef = np.arange(1, self.M + 1).reshape(-1, 1)
-        imaj_cos_theta_coef = np.cos(theta * imaj_cos_coef)
-        imaj = np.square(np.dot(self.a_coef.reshape(1, -1), imaj_cos_theta_coef))
-
-        self.freq_resp = 1 / np.sqrt(real + imaj)
-        self.theta = theta
-
-    def process_all(self, M):
-        self.find_rxx_matrix(M)
-        self.find_a_coef()
-        self.find_e_m()
-        self.find_x_predict()
-        self.find_error_modelling()
-        self.find_mse()
+        self.theta, self.freq_resp = _mag_theta(self.M, self.a_coef)
 
     def to_sound(self):
-        raw_data= self.raw_data_y.flatten()
-        predicted_data = self.x_predict.flatten()
-        min_r_d = np.min(raw_data)
-        max_r_d =np.max(raw_data)
-        min_p_d = np.min(predicted_data)
-        max_p_d = np.max(predicted_data)
-        new_max = 2147483647
-        new_min = -2147483648
-        
-        norm_raw_data=(raw_data-min_r_d)/(max_r_d-min_r_d)*(new_max-new_min)+new_min
-        norm_predicted_data=(predicted_data-min_p_d)/(max_p_d-min_p_d)*(new_max-new_min)+new_min
-        
-        wav.write(os.path.join("Data","raw_data.wav"), 3000, norm_raw_data.astype(np.int32))
-        wav.write(os.path.join("Data","predicted_data.wav"), 3000, norm_predicted_data.astype(np.int32))
+        norm_raw_data, norm_predicted_data = _norm_to_wav(
+            self.raw_data_y, self.x_predict
+        )
+
+        wav.write(
+            os.path.join("Data", "raw_data.wav"), 3000, norm_raw_data.astype(np.int32)
+        )
+        wav.write(
+            os.path.join("Data", "predicted_data.wav"),
+            3000,
+            norm_predicted_data.astype(np.int32),
+        )
--- frontend_linear_prediction.py
+++ frontend_linear_prediction.py
@@ -1,5 +1,6 @@
+# from pandas.core.frame import DataFrame
 import streamlit as st
-import pandas as pd
+from pandas import DataFrame as df
 import plotly.express as px
 import numpy as np
 import os
@@ -42,49 +43,48 @@ class Main:
         st.title("Linear Prediction Model")
         # Class Initialization
         data_path = os.path.join("Data", "Print_13_v2_PCG_RV.txt")
-        linear_model = LinearModelPrediction(data_path)
+        self.linear_model = LinearModelPrediction(data_path)
 
         self.color = ["#8CDFD6", "#1E555C", "#F4D8CD", "#EDB183", "#F15152", "#FFFFFF"]
         # Raw Data Plot
-        self.plot_raw_data(linear_model)
+        st.header("Raw Signal")
+        self.plot_raw_data()
 
         if st.session_state["run"]:
-            style_format = "{:.5f}"
-
+            self.style_format = "{:.5f}"
+            self.dtype = np.float16
             # Find rxx and RXX
-            self.find_rxx_Rxx(linear_model, style_format)
+            self.find_rxx_Rxx()
 
             # Find a coefficient
-            self.find_a_coef(linear_model, style_format)
+            self.find_a_coef()
 
             #  Find e(m) (error)
-            self.find_e_m(linear_model, style_format)
+            self.find_e_m()
 
             # Plot Raw Data with e(m)
-            self.plot_raw_em(linear_model)
+            self.plot_raw_em()
 
             # Predict New data
-            self.find_x_predict(linear_model)
+            self.find_x_predict()
 
             # Find Modelling Error and MSE
-            self.find_err_mod_mse(linear_model)
+            self.find_err_mod_mse()
 
             # Freq Resp
-            self.find_freq_resp(linear_model)
+            self.find_freq_resp()
             # Compare Raw and Predicted Signal
-            self.compare(linear_model)
-            
-            
-            
+            self.compare()
 
-    def find_freq_resp(self, linear_model):
-        linear_model.find_freq_resp()
+    def find_freq_resp(self):
+        self.linear_model.find_freq_resp()
         st.subheader("Frequency Response")
-        freq_resp_df = pd.DataFrame(
+        freq_resp_df = df(
             {
-                "Frequency (Ohm)": linear_model.theta.flatten(),
-                "Magnitude (dB)": linear_model.freq_resp.flatten(),
-            }
+                "Frequency (Ohm)": self.linear_model.theta.flatten(),
+                "Magnitude (dB)": self.linear_model.freq_resp.flatten(),
+            },
+            dtype=self.dtype,
         )
         freq_resp_fig = px.line(
             freq_resp_df,
@@ -95,14 +95,15 @@ class Main:
         freq_resp_fig.update_layout(yaxis_title="Magnitude (dB)")
         st.plotly_chart(freq_resp_fig, use_container_width=True)
 
-    def compare(self, linear_model):
-        linear_model.to_sound()
+    def compare(self):
+        self.linear_model.to_sound()
         st.subheader("Raw and Predicted Signal Comparison")
-        compare_df = pd.DataFrame(
+        compare_df = df(
             {
-                "Raw Signal": linear_model.raw_data_y,
-                "Predicted Signal": linear_model.x_predict.flatten(),
-            }
+                "Raw Signal": self.linear_model.raw_data_y,
+                "Predicted Signal": self.linear_model.x_predict.flatten(),
+            },
+            dtype=self.dtype,
         )
         compare_fig = px.line(
             compare_df,
@@ -112,22 +113,23 @@ class Main:
         )
 
         st.plotly_chart(compare_fig, use_container_width=True)
-        st.plotly_chart(self.raw_data_fig, use_container_width=True)
-        st.audio(os.path.join("Data","raw_data.wav"))
+        st.plotly_chart(self.raw_data_fig_func(), use_container_width=True)
+        st.audio(os.path.join("Data", "raw_data.wav"))
         st.plotly_chart(self.predict_data_fig, use_container_width=True)
-        st.audio(os.path.join("Data","predicted_data.wav"))
+        st.audio(os.path.join("Data", "predicted_data.wav"))
 
-    def find_err_mod_mse(self, linear_model):
-        linear_model.find_error_modelling()
-        linear_model.find_mse()
+    def find_err_mod_mse(self):
+        self.linear_model.find_error_modelling()
+        self.linear_model.find_mse()
 
         st.subheader("Modeling Error")
-        st.markdown(f"##### MSE: {linear_model.mse}")
-        err_mod_df = pd.DataFrame(
+        st.markdown(f"##### MSE: {self.linear_model.mse}")
+        err_mod_df = df(
             {
-                "t (second)": linear_model.raw_data_x,
-                "Modelling Error": linear_model.error_modelling.flatten(),
-            }
+                "t (second)": self.linear_model.raw_data_x,
+                "Modelling Error": self.linear_model.error_modelling.flatten(),
+            },
+            # dtype=self.dtype,
         )
         err_mod_fig = px.line(
             err_mod_df,
@@ -138,14 +140,15 @@ class Main:
         err_mod_fig.update_layout(yaxis_title="V (mv)")
         st.plotly_chart(err_mod_fig, use_container_width=True)
 
-    def find_x_predict(self, linear_model):
-        linear_model.find_x_predict()
+    def find_x_predict(self):
+        self.linear_model.find_x_predict()
         st.subheader("Predicted Signal")
-        predict_data_df = pd.DataFrame(
+        predict_data_df = df(
             {
-                "t (second)": linear_model.raw_data_x,
-                "Predicted Data": linear_model.x_predict.flatten(),
-            }
+                "t (second)": self.linear_model.raw_data_x,
+                "Predicted Data": self.linear_model.x_predict.flatten(),
+            },
+            dtype=self.dtype,
         )
         predict_data_fig = px.line(
             predict_data_df,
@@ -157,10 +160,14 @@ class Main:
         self.predict_data_fig = predict_data_fig
         st.plotly_chart(predict_data_fig, use_container_width=True)
 
-    def plot_raw_data(self, linear_model):
-        st.header("Raw Signal")
-        raw_data_df = pd.DataFrame(
-            {"t (second)": linear_model.raw_data_x, "Raw Data": linear_model.raw_data_y}
+    @st.cache(max_entries=10, ttl=600)
+    def raw_data_fig_func(self):
+        raw_data_df = df(
+            {
+                "t (second)": self.linear_model.raw_data_x.flatten(),
+                "Raw Data": self.linear_model.raw_data_y.flatten(),
+            },
+            # dtype=self.dtype,
         )
         raw_data_fig = px.line(
             raw_data_df,
@@ -169,17 +176,20 @@ class Main:
             color_discrete_sequence=[self.color[0]],
         )
         raw_data_fig.update_layout(yaxis_title="V (mv)")
-        self.raw_data_fig = raw_data_fig
-        st.plotly_chart(raw_data_fig, use_container_width=True)
+        return raw_data_fig
 
-    def plot_raw_em(self, linear_model):
+    def plot_raw_data(self):
+        st.plotly_chart(self.raw_data_fig_func(), use_container_width=True)
+
+    def plot_raw_em(self):
         st.subheader("Raw Data & e(m) Plot")
-        raw_data_em_df = pd.DataFrame(
+        raw_data_em_df = df(
             {
-                "t (second)": linear_model.raw_data_x,
-                "Raw Data": linear_model.raw_data_y.flatten(),
-                "e(m)": linear_model.e_m.flatten(),
-            }
+                "t (second)": self.linear_model.raw_data_x,
+                "Raw Data": self.linear_model.raw_data_y.flatten(),
+                "e(m)": self.linear_model.e_m.flatten(),
+            },
+            dtype=self.dtype,
         )
         raw_data_em_fig = px.line(
             raw_data_em_df,
@@ -190,59 +200,67 @@ class Main:
         raw_data_em_fig.update_layout(yaxis_title="V (mv)")
         st.plotly_chart(raw_data_em_fig, use_container_width=True)
 
-    def find_e_m(self, linear_model, style_format):
-        linear_model.find_e_m()
-        cols_3 = st.columns(2)
-        # with cols_3[0]:
-        st.subheader("e(m)")
-        em_df = pd.DataFrame(linear_model.e_m, dtype=np.float16)
-        st.dataframe(em_df.style.format(style_format))
+    def find_e_m(self):
+        self.linear_model.find_e_m()
 
-    def find_a_coef(self, linear_model, style_format):
-        linear_model.find_a_coef()
+        st.subheader("e(m)")
+        em_df = df(
+            self.linear_model.e_m,
+            dtype=self.dtype,
+        )
+        st.dataframe(em_df.style.format(self.style_format))
+
+    def find_a_coef(
+        self,
+    ):
+        self.linear_model.find_a_coef()
         cols_2 = st.columns(2)
         with cols_2[0]:
             st.subheader("Invers Rxx")
             if not st.session_state["no_df"]:
-                inv_Rxx_df = pd.DataFrame(
-                    linear_model.inv_rxx_matrix,
-                    index=np.arange(linear_model.inv_rxx_matrix.shape[0]).astype(str),
-                    columns=np.arange(linear_model.inv_rxx_matrix.shape[1]).astype(str),
-                    dtype=np.float16,
+                inv_Rxx_df = df(
+                    self.linear_model.inv_rxx_matrix.astype(np.float32),
+                    index=np.arange(self.linear_model.inv_rxx_matrix.shape[0]).astype(np.uint16),
+                    columns=np.arange(self.linear_model.inv_rxx_matrix.shape[1]).astype(np.uint16),
+                    dtype=self.dtype,
                 )
-                st.dataframe(inv_Rxx_df.style.format(style_format))
+                st.dataframe(inv_Rxx_df.style.format(self.style_format))
             else:
                 st.write("Hidden For Better Rendering Performance")
 
         with cols_2[1]:
             st.subheader("a Coefficient")
-            a_coef_df = pd.DataFrame(
-                linear_model.a_coef,
-                index=[str(i) for i in np.arange(1, linear_model.a_coef.shape[0] + 1)],
+            a_coef_df = df(
+                self.linear_model.a_coef,
+                index=np.arange(len(self.linear_model.a_coef.flatten())).astype(np.uint16),
                 columns=["a"],
-                dtype=np.float16,
+                dtype=self.dtype,
             )
-            st.dataframe(a_coef_df.style.format(style_format))
+            st.dataframe(a_coef_df.style.format(self.style_format))
 
-    def find_rxx_Rxx(self, linear_model, style_format):
-        linear_model.find_rxx_matrix(st.session_state["time_lag"])
+    def find_rxx_Rxx(self):
+        self.linear_model.find_rxx_matrix(st.session_state["time_lag"])
 
         cols_1 = st.columns(2)
         with cols_1[0]:
             st.subheader("rxx")
-            rxx_df = pd.DataFrame({"rxx": linear_model.rxx.flatten()})
-            st.dataframe(rxx_df.style.format(style_format))
-
+            rxx_df = df(
+                self.linear_model.rxx.flatten().astype(np.float32),
+                columns=["rxx"],
+                index = np.arange(len(self.linear_model.rxx.flatten())).astype(np.uint16),
+                dtype=self.dtype,
+            )
+            st.dataframe(rxx_df.style.format(self.style_format))
         with cols_1[1]:
             st.subheader("Rxx")
             if not st.session_state["no_df"]:
-                Rxx_df = pd.DataFrame(
-                    linear_model.rxx_matrix,
-                    index=np.arange(linear_model.rxx_matrix.shape[0]).astype(str),
-                    columns=np.arange(linear_model.rxx_matrix.shape[1]).astype(str),
-                    dtype=np.float16,
+                Rxx_df = df(
+                    self.linear_model.rxx_matrix.astype(np.float32),
+                    index=np.arange(self.linear_model.rxx_matrix.shape[0]).astype(np.uint16),
+                    columns=np.arange(self.linear_model.rxx_matrix.shape[1]).astype(np.uint16),
+                    dtype=self.dtype,
                 )
-                st.dataframe(Rxx_df.style.format(style_format))
+                st.dataframe(Rxx_df)
             else:
                 st.write("Hidden For Better Rendering Performance")
 
