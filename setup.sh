68a136ea754b2fdb586201feeff915a0f8ad9772
Aditya Wardianto <adityawardianto.19073@mhs.its.ac.id>
2021-10-13 10:21:51 +0700

    Test setup.sh

--- setup.sh
+++ setup.sh
@@ -4,8 +4,9 @@ echo "\
 headless = true\n\
 port = $PORT\n\
 \n\
+\
 [theme]\n\
-base="dark\n\"
+base="dark"\n\
 primaryColor="#ffffff"\n\
 backgroundColor="#000000"\n\
 secondaryBackgroundColor="#000000"\n\
